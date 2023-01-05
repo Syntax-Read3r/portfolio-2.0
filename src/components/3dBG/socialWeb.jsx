d4e50688e1765f2bf003be937f7722036daae5bb
Munyaradzi <syntaxread3r@gmail.com>
2023-01-05 23:22:49 +0000

    initial commit

---
+++ src/components/3dBG/socialWeb.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Spline from '@splinetool/react-spline';
+
+function socialWeb() {
+  return (<>
+    <Spline scene="https://prod.spline.design/6RhseRSW-hQNARGX/scene.splinecode" />
+    </>
+  );
+}
+
+export default socialWeb
